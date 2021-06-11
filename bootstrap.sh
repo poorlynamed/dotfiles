419a60f9cf2302857acdaf49066b0822d3963a4f
poorlynamed <5681733+poorlynamed@users.noreply.github.com>
2021-06-10 21:34:00 -0700

    fixed some crap with pop_os

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,10 +19,12 @@ linuxPkgs=('git' 'neovim' 'nodejs' 'npm' 'golang' 'tmux' 'docker' 'coreutils' 'w
   [[ $(id -u) == 0 ]] || { echo Must run as root; exit 1; };                                          \
   echo 'installing packages';                                                                         \
   grep -iq ubuntu /etc/issue && apt install -y ${linuxPkgs[@]} bind9utils build-essential;            \
+  grep -iq 'Pop!_OS' /etc/issue && apt install -y ${linuxPkgs[@]} bind9utils build-essential;            \
   grep -iq arch /etc/issue && apt install -y ${linuxPkgs[@]} base-devel;                              \
 }
 
 echo 'installing oh-my-zsh'
+su $SUDO_USER
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 exit 0
--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ backupDir=~/.dotfiles_backup-$(date +'%s')
 mkdir $backupDir || exit 1
 
 for cfgFile in $(ls -ap | grep -Ev '/|^\.|install.sh|bootstrap.sh'); do
-  [[ -e ~/.$cfgFile ]] && mv ~/.$cfgFile $backupDir/$cfgFile
-  cp $cfgFile ~/.$cfgfile
+  [[ -e ~/.$cfgFile ]] && mv "~/.$cfgFile" "$backupDir/$cfgFile"
+  cp $cfgFile ~
+  cd ~
+  mv "$cfgFile" ".$cfgFile"
 done
 
 printf -- "done -- original configs backed up to %s\n" "$backupDir"
