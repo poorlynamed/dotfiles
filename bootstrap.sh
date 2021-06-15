6783a122b61a02fd0d46ad6b0967b8afa3af333b
poorlynamed <5681733+poorlynamed@users.noreply.github.com>
2021-06-15 16:43:57 -0700

    fixed install.sh; made bootstrap.sh run oh-my-zsh installer as sudo_user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#shellcheck disable=2068
 
 osxPkgs=('git' 'neovim' 'nodejs' 'npm' 'golang' 'viscosity' 'tmux' 'lulu' 'rectangle'                 \
          'easy-move-plus-resize' 'firefox' 'docker' 'coreutils' 'wget' 'weechat' 'zsh')
@@ -11,7 +12,7 @@ linuxPkgs=('git' 'neovim' 'nodejs' 'npm' 'golang' 'tmux' 'docker' 'coreutils' 'w
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"     \
     || exit 1;                                                                                        \
   echo 'installing packages';                                                                         \
-  brew install ;                                                                                      \
+  brew install ${osxPkgs[@]};                                                                         \
 }
 
 # If this is linux, figure out pkg manager & install common pkg's
@@ -19,12 +20,12 @@ linuxPkgs=('git' 'neovim' 'nodejs' 'npm' 'golang' 'tmux' 'docker' 'coreutils' 'w
   [[ $(id -u) == 0 ]] || { echo Must run as root; exit 1; };                                          \
   echo 'installing packages';                                                                         \
   grep -iq ubuntu /etc/issue && apt install -y ${linuxPkgs[@]} bind9utils build-essential;            \
-  grep -iq 'Pop!_OS' /etc/issue && apt install -y ${linuxPkgs[@]} bind9utils build-essential;            \
+  grep -iq 'Pop!_OS' /etc/issue && apt install -y ${linuxPkgs[@]} bind9utils build-essential;         \
   grep -iq arch /etc/issue && apt install -y ${linuxPkgs[@]} base-devel;                              \
 }
 
-echo 'installing oh-my-zsh'
-su $SUDO_USER
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+printf 'installing oh-my-zsh for user: %s\n' "$SUDO_USER"
+sudo -u "$SUDO_USER" -H \
+  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 exit 0
--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-backupDir=~/.dotfiles_backup-$(date +'%s')
-mkdir $backupDir || exit 1
+GLOBIGNORE=".:.."
 
-for cfgFile in $(ls -ap | grep -Ev '/|^\.|install.sh|bootstrap.sh'); do
-  [[ -e ~/.$cfgFile ]] && mv "~/.$cfgFile" "$backupDir/$cfgFile"
-  cp $cfgFile ~
-  cd ~
-  mv "$cfgFile" ".$cfgFile"
+# set up backup directory
+backupDir="$HOME/.dotfiles_backup-$(date +'%s')"
+mkdir "$backupDir" || exit 1
+
+for cfgFile in *; do
+  # skip dotfiles & install/bootstrap scripts
+  [[ $cfgFile =~ (install.sh|bootstrap.sh|.git|.gitignore) ]] && continue
+
+  # if dotfile exists in home dir, move it to backup folder
+  [[ -f $HOME/.$cfgFile ]] && mv "$HOME/.$cfgFile" "$backupDir/$cfgFile"
+  cp "$cfgFile" "$HOME/.$cfgFile" && printf "Copied ~/.%s\n" "$cfgFile"
 done
 
 printf -- "done -- original configs backed up to %s\n" "$backupDir"
